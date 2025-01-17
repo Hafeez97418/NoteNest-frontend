cc26bc7859b8ebe0ee3f9b4d8c960617105a1754
Hafeez <muhammed.hafeez.nayazi@gmail.com>
2025-01-17 11:07:34 +0530

    navbar and footer created

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
+import daisyui from "daisyui";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*"],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
-
+  plugins: [daisyui],
+};
